4b69c1d919487ed4558692082482283f79aeb48b
clarke newsham <clarkenewsham@clarkes-MacBook-Pro-2.local>
2022-02-15 11:21:32 +0000

    fetch all beers request & map all beers, refactor app comonents to and created serperate beer components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { useState, useEffect } from 'react';
 const App = () => {
 
   const [randomBeer, setRandomBeer] = useState('');
+  const [allBeers, setAllBeers] = useState('');
   const [isError, setIsError] = useState({
     error: false,
     message: ''
   });
 
+  // fetch single random beer
   const handleData = async () => {
     try {
       const response = await fetch('https://api.punkapi.com/v2/beers/random');
@@ -31,14 +33,44 @@ const App = () => {
     }
   }
 
-  // use effect hook to load advice response data on page load - using [] dependency to update on load
+
+  // fetch all beers
+  const handleAllData = async () => {
+    try {
+      const response = await fetch('https://api.punkapi.com/v2/beers');
+       console.log(response);
+
+      if(response.status !== 200) {
+        console.log("error");
+        throw new Error ('oooops...could not return data');
+      }
+       // parse data into json
+       const data = await response.json();
+      //  console.log(data[0].name);
+       setAllBeers(data);
+       console.log(allBeers);
+
+    // catch errors that occur in the try - throw error message generated from the throw new Error
+    } catch(error) {
+      setIsError({ error: true, message: error.message})
+      console.log(isError);
+    }
+  }
+
+  // use effect hook to load random beers and allBeers response data on page load - using [] dependency to update on load
   useEffect(() => {
     handleData();
+    handleAllData();
   }, [])
 
   
   if(isError.error) {
     return <div>{isError.message}</div>
+  }
+
+  // loading condition to ensure that the data has time to be returned before the beers data is rendered
+  if(!randomBeer) {
+    return <h2>Loading....</h2>
   }
 
   return (
@@ -48,7 +80,16 @@ const App = () => {
       <h6>Todays random brewdog is: {randomBeer.description}</h6>
       <img src={randomBeer.image_url} />
       <button onClick={handleData}>Get random beer</button>
+      {allBeers.map((beer, index) => {
+        return (
+          <div key={index}>
+            <h1>{beer.name}</h1>
+            <h4>{beer.tagline}</h4>
+        </div>
+        ) 
+      })}
     </div>
+
   )
 }
 export default App;
---
+++ src/components/RandomBeersList.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RandomBeersList = () => {
+  return <div></div>;
+};
+
+export default RandomBeersList;
